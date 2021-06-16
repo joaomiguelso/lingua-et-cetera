206b74738110ce8efb82f8af5d5b870f160aac80
Joao Oliveira <joaomiguelso@hotmail.com>
2021-06-16 17:06:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import HomeImg from "./assets/joshua-hoehne.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <navbar className="navbar">
+          <h2 className="home-title">Lingua Et Cetera</h2>
+          <div>
+            <span className="navbar-link">About</span>
+            <span className="navbar-link">Services</span>
+            <span className="navbar-link">Stories</span>
+            <span className="navbar-link">About</span>
+          </div>
+        </navbar>
+        <img src={HomeImg} className="home-img fade-in" />
+
+        <div className="home-container">
+          <div>
+            <h4 className="home-subtitle">About this website</h4>
+            <p>
+              Text about this website Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+          </div>
+          <div>
+            <h4 className="home-subtitle">About myself</h4>
+            <p>
+              Text about this myself Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+          </div>
+        </div>
+        <footer>© 2021 Lingua Et Cetera. All Rights Reserved</footer>
+      </div>
     </div>
   );
 }
